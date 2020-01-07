60af4e9ce74c040b07e500cfaadf9d4e2c61269e
jacksonwjc99 <weijie11@hotmail.com.tw>
2020-01-07 11:49:19 +0800

    commited

--- app/src/main/java/com/example/mobileappassignment/MainActivity.kt
+++ app/src/main/java/com/example/mobileappassignment/MainActivity.kt
@@ -14,15 +14,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-
         drawerLayout = binding.drawerLayout
-
         val navController = this.findNavController(R.id.myNavHostFragment)
-
         NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
-
         NavigationUI.setupWithNavController(binding.navView, navController)
-
     }
 
     override fun onSupportNavigateUp(): Boolean {
